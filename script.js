4e6a12c176a570f069673a5da6830a30443438a2
Satch00 <119885991+Satch00@users.noreply.github.com>
2025-02-19 10:24:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function Join(){
+    var elemento = document.querySelector('.info-games');
+    if (elemento.style.visibility == 'hidden'){
+        elemento.style.visibility = 'visible';
+    }else{
+        elemento.style.visibility = 'hidden';
+    }
+}
