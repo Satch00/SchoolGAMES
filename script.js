913ef358cb7d74fb65f408537de5e6d775c8088f
Satch00 <119885991+Satch00@users.noreply.github.com>
2025-02-20 00:03:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function Join(){
+function doom(){
+    window.location.href = 'https://satch00.github.io/SchoolGAMES/doom.pdf'
+}
+
+function Join(element){
     var elemento = document.querySelector('.info-games');
     if (elemento.style.visibility == 'hidden'){
+        console.log("o botão com id " + element.querySelector('img').id + " foi aberto")
         elemento.style.visibility = 'visible';
+        if ( element.querySelector('img').id == "game3"){
+            doom()
+        }
     }else{
+        console.log("o botão com id " + element.querySelector('img').id + " foi fechado")
         elemento.style.visibility = 'hidden';
     }
 }
+
+/**     
+function DoomDownload(){    
+}
+*/
