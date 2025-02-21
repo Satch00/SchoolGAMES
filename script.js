6127135aa617f193bf51cc4993662631422c7c33
Satch00 <119885991+Satch00@users.noreply.github.com>
2025-02-21 11:11:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function butt(){
         case "game5":
                 console.log("Abrindo Celeste Classic");
                 window.location.href = 'https://satch00.github.io/SchoolGAMES/celeste_practice_mode.html';
+                break;
     }
 }
 
